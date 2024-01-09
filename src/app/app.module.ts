bbadb2d8e0677ab8793181f5075cc5db7d573f3d
Aman07a <aman16.aa17@gmail.com>
2024-01-09 14:01:07 +0100

    Building a simple navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HomeModule } from './home/home.module';
+
 import { AppComponent } from './app.component';
+import { ReservationModule } from './reservation/reservation.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HomeModule, ReservationModule],
   providers: [],
   bootstrap: [AppComponent],
 })
